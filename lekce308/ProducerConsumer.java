30b452678d9a21b6048d6d7b5ae3783222629b09
Filip Edlund <90387737+FilipEdlund01@users.noreply.github.com>
2022-03-03 09:17:50 +0100

    Add files via upload

---
+++ lekce308/ProducerConsumer.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lekce308;
+
+/**
+ *
+ * @author 2019-e-edlund
+ */
+public class ProducerConsumer {
+    private final int[] data = new int[100];
+    
+}
